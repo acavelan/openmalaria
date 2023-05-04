2eb11c3ca97350bd47010f388c9e99ea6fedcaed
Aurélien Cavelan <aurelien.cavelan@unibas.ch>
2023-05-04 11:08:58 -0500

    merge PerHostAnoph into PerHost

--- model/Transmission/Anopheles/PerHostAnoph.h
+++ model/Transmission/Anopheles/PerHostAnoph.h
@@ -90,55 +90,6 @@ private:
     static vector<PerHostAnophParams> params;
 };
 
-/** Data needed for each human which is per-mosquito species. */
-class PerHostAnoph
-{
-public:
-    /** In lieu of a constructor initialises elements, using the passed base to
-     * get baseline parameters. */
-    void initialise (LocalRng& rng, size_t species, double availabilityFactor);
-    
-    /// Checkpointing
-    template<class S>
-    void operator& (S& stream) {
-        entoAvailability & stream;
-        probMosqBiting & stream;
-        probMosqRest & stream;
-    }
-    
-    /** Return the availability rate (α_i) of this human to mosquitoes. */
-    inline double getEntoAvailability() const {
-        return entoAvailability;
-    }
-    /** Return the probability of mosquito successfully biting host (P_B_i) */
-    inline double getProbMosqBiting() const {
-        return probMosqBiting;
-    }
-    /** Return the probability of mosquito escaping human and finding a resting
-     * site, then resting without dying, after biting the human (P_C_i * P_D_i). */
-    inline double getProbMosqRest() const {
-        return probMosqRest;
-    }
-    
-private:
-    ///@brief Rate/probabilities before interventions. See functions.
-    //@{
-    /** Availability rate of human to mosquitoes, including hetergeneity factor
-     * and base rate, but excluding age and intervention factors. */
-    double entoAvailability;
-    
-    /** Probability of mosquito successfully biting host (P_B_i) in the absense of
-     * interventions. */
-    double probMosqBiting;
-    
-    /** Probability of mosquito escaping human and finding a resting site, then
-     * resting without dying, after biting the human (P_C_i * P_D_i) in the
-     * absense of interventions. */
-    double probMosqRest;
-    //@}
-};
-
-
 }
 }
 }
